cf16b980bcce81b2a51ffb0262d49df693fc6983
Furkan Doğar <furkandogar@hotmail.com>
2021-01-31 00:18:09 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //The classes that implmement IEntity are database tables.
+    public interface IEntity
+    {
+    }
+}
